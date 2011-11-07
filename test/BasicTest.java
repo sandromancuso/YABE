817ae531ed0b76158be95d9e7e7aba96f349dab5
Sandro Mancuso <sandromancuso@yahoo.com>
2011-11-07 00:21:27 +0000

    Initial commit

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -5,9 +5,58 @@ import models.*;
 
 public class BasicTest extends UnitTest {
 
-    @Test
-    public void aVeryImportantThingToTest() {
-        assertEquals(2, 1 + 1);
-    }
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setup() {
+		Fixtures.deleteAll();
+	}
+
+	@Test
+	public void createAndRetrieveUser() {
+		// Create a new user and save it.
+		new User("bob@gmail.com", "secret", "Bob").save();
+
+		// Retrieve the user with e-mail address bob@gmail.com
+		User bob = User.find("byEmail", "bob@gmail.com").first();
+
+		// Test
+		assertNotNull(bob);
+		assertEquals("Bob", bob.fullname);
+	}
+
+	@Test
+	public void tryConnectAsUser() {
+		// Create a new user and save it
+		new User("bob@gmail.com", "secret", "Bob").save();
+
+		// Test
+		assertNotNull(User.connect("bob@gmail.com", "secret"));
+		assertNull(User.connect("bob@gmail.com", "badpassword"));
+		assertNull(User.connect("tom@gmail.com", "secret"));
+	}
+	
+	@Test
+	public void createPost() {
+	    // Create a new user and save it
+	    User bob = new User("bob@gmail.com", "secret", "Bob").save();
+	    
+	    // Create a new post
+	    new Post(bob, "My first post", "Hello world").save();
+	    
+	    // Test that the post has been created
+	    assertEquals(1, Post.count());
+	    
+	    // Retrieve all posts created by Bob
+	    List<Post> bobPosts = Post.find("byAuthor", bob).fetch();
+	    
+	    // Tests
+	    assertEquals(1, bobPosts.size());
+	    Post firstPost = bobPosts.get(0);
+	    assertNotNull(firstPost);
+	    assertEquals(bob, firstPost.author);
+	    assertEquals("My first post", firstPost.title);
+	    assertEquals("Hello world", firstPost.content);
+	    assertNotNull(firstPost.postedAt);
+	}	
 
 }

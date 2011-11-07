e07f0738cc1ec2173020b55210d6aa5b0771675e
Sandro Mancuso <sandromancuso@yahoo.com>
2011-11-07 08:53:39 +0000

    Post comments model.

--- app/models/Post.java
+++ app/models/Post.java
@@ -17,11 +17,22 @@ public class Post extends Model {
     @ManyToOne
     public User author;
     
+    @OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+    public List<Comment> comments;    
+    
     public Post(User author, String title, String content) {
+        this.comments = new ArrayList<Comment>();
         this.author = author;
         this.title = title;
         this.content = content;
         this.postedAt = new Date();
     }
+    
+    public Post addComment(String author, String content) {
+        Comment newComment = new Comment(this, author, content).save();
+        this.comments.add(newComment);
+        this.save();
+        return this;
+    }    
  
 }

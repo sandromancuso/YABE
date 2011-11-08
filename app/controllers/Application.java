d881fa010367ddf9ddd66b59d1b455f72862488a
Sandro Mancuso <sandromancuso@yahoo.com>
2011-11-08 01:58:51 +0000

    Html template, layout and css.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,15 +9,17 @@ import models.*;
 
 public class Application extends Controller {
 
+	@Before
+	static void addDefaults() {
+		renderArgs.put("blogTitle", Play.configuration.getProperty("blog.title"));
+		renderArgs.put("blogBaseline", Play.configuration.getProperty("blog.baseline"));
+	}
+	
     public static void index() {
     	Post frontPost = Post.find("order by postedAt desc").first();
     	List<Post> olderPosts = Post.find("order by postedAt desc").from(1).fetch(10);
         render(frontPost, olderPosts);
     }
-    
-    public static void users() {
-    	List<User> users = User.findAll();
-    	renderJSON(users);
-    }
+
 
 }
